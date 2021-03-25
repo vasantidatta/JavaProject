714e8d46c74e1adf864dad4e5edd47c4a0a97dad
vasanti <vasantidatta@gmail.com>
2021-03-25 00:34:21 -0400

    string methods modified

--- src/basicsofjava2/StringMethods.java
+++ src/basicsofjava2/StringMethods.java
@@ -6,11 +6,12 @@ public class StringMethods {
 		
 		
 		String str="my name is vasanti";
+		String str1 = "I am cool";
 		
 		System.out.println(str.length());
 		System.out.println(str.charAt(5));
 		System.out.println(str.charAt(12));
-		
+		System.out.println(str+" "+str1);		
 		
 		
 
