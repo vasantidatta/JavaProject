b873e1f017bbecf97f145bd96e784a55a7d414c0
vasanti <vasantidatta@gmail.com>
2021-03-26 15:15:58 -0400

    modified File_handling

--- src/fileio/File_handling.java
+++ src/fileio/File_handling.java
@@ -1,6 +1,10 @@
 package fileio;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class File_handling {
@@ -13,7 +17,38 @@ public class File_handling {
 			
 			System.out.println("file has been created");
 		}
-
-	}
+		
+		FileOutputStream fout = new FileOutputStream(file);
+		
+		String str = "Selenium is open source testing tool for web based applications";
+		
+		byte[] b = str.getBytes();
+		fout.write(b);
+		fout.close();
+		
+		FileInputStream fis = new FileInputStream(file);
+		int i;
+		while((i=fis.read())!=-1) {
+			System.out.print((char)i);
+		}
+		System.out.println("");
+		
+        String msg = " cucumber is behavior driven development framework";
+        
+        byte[] bt = msg.getBytes();
+        
+        FileWriter write = new FileWriter(file);
+        
+        write.append(msg);
+        
+        FileReader read = new FileReader(file);
+        
+        int j;
+        
+        while((j=read.read())!=-1) {
+        	System.out.print((char)j);
+        }
+        System.out.println("");
+}
 
 }
